70d064c93abfa89eb0ea5f2cbc3cb7f71eb452c6
Justus <justuskwildenradt@gmail.com>
2018-06-13 10:13:40 -0700

    Receiving reviewBundle on get request

--- client/index.jsx
+++ client/index.jsx
@@ -1,42 +1,47 @@
-import react from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import axios from 'axios';
 
-export default class FullApp extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      reviews: '',
-      roomId: window.location.pathname.split('/')[2],
-    };
-  }
+// export default class FullApp extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       reviews: '',
+//       roomId: window.location.pathname.split('/')[2],
+//     };
+//   }
 
-  componentDidMount() {
-    this.getComponents();
-  }
+//   componentDidMount() {
+//     this.getComponents();
+//   }
 
-  getComponents() {
-    axios.get('/rooms/:id',{
-      params: {
-        ID: this.state.roomId
-      }
-    })
-      .then((response) => {
-        this.setState({
-          reviews: response.data,
-        })
-      })
-      .catch(error => console.error('failed to get reviews', error));
-  }
+//   getComponents() {
+//     axios.get('/rooms/:id',{
+//       params: {
+//         ID: this.state.roomId
+//       }
+//     })
+//       .then((response) => {
+//         console.log(response.data)
+//         // const review = response.data.split('<body>', '</body>')[1];
+//         console.log('review', review)
+//         this.setState({
+//           reviews: response.data,
+//         })
+//       })
+//       .catch(error => console.error('failed to get reviews', error));
+//   }
 
-  render() {
-    return (
-      <div>
-        <div>hello</div>
-        <div>{this.state.reviews}</div>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <div>hello</div>
+//         {/* <div>{this.state.reviews}</div> */}
+//         {/* <div id="reviewIndex"></div>
+//         <script src="http://localhost:3001/reviewBundle.js"></script>  */}
+//       </div>
+//     );
+//   }
+// }
 
-ReactDOM.render(<FullApp />, document.getElementById('airFeC'));
+// ReactDOM.render(<FullApp />, document.getElementById('airFeC'));
--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/reviews', (req, res) => {
-
+app.get('/rooms/:id', (req, res) => {
+  axios.get('http://localhost:3001/rooms/:id/reviewBundle.js', {
+    params: {
+      ID: 5,
+    }
+  })
+    .then((response) => {
+      console.log('got response in proxy', response.data)
+      //const data = JSON.parse(response.data);
+      res.send(response.data);
+    })
+    .catch(error => console.error('no response in proxy', error))
 });
 
 app.listen(port, () => {
