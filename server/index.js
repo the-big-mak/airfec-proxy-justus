b9027b3d4c1f1de27729eb064d9c416b460b3ebd
Justus <justuskwildenradt@gmail.com>
2018-06-17 18:14:49 -0700

    Connected proxy server to EC2 instances for reviews and reservations

--- server/index.js
+++ server/index.js
@@ -7,38 +7,26 @@ const port = 3000;
 
 app.use('/rooms/:id', express.static(path.join(__dirname, '../public')));
 
-// app.get('/reviews', (req, res) => {
-//   axios.get('http://localhost:3001/reviews', {
-//     params: {
-//       id: req.query.id,
-//     }
-//   })
-//     .then((response) => res.send(response.data))
-//     .catch(error => console.error('no response in proxy', error))
-// });
-
-// app.get('/description', (req, res) => {
-//   axios.get('http://localhost:3002/description', {
-//     params: {
-//       id: req.query.id,
-//     }
-//   })
-//     .then((response) => res.send(response.data))
-//     .catch(error => console.error('no response in proxy', error))
-// });
+app.get('/description', (req, res) => {
+  axios.get('http://localhost:3002/description', {
+    params: {
+      id: req.query.id,
+    }
+  })
+    .then((response) => res.send(response.data))
+    .catch(error => console.error('no response in proxy', error))
+});
 
 app.get('/reviews/:id', (req, res) => {
-  res.redirect(`http://0.0.0.0:3001/reviews/${req.params.id}`);
+  res.redirect(`http://ec2-54-173-131-192.compute-1.amazonaws.com/reviews/${req.params.id}`);
 });
-app.get('/description', (req, res) => {
-  res.redirect(`http://localhost:3002/description/${req.params.id}`);
-});
+
 app.get('/Photos/:id', (req, res) => {
   res.redirect(`http://localhost:3003/Photos/${req.params.id}`);
 });
 
 app.get('/reservations/:id', (req, res) => {
-  res.redirect(`http://localhost:3004/reservations/${req.params.id}`);
+  res.redirect(`http://ec2-18-208-163-155.compute-1.amazonaws.com/reservations/${req.params.id}`);
 });
 
 app.listen(port, () => {
--- server/routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  reviews: 'http://localhost:3001/reviews',
-  reservations: 'http://localhost:3004/rooms:id',
-  description: 'http://localhost:3001/rooms:id',
-  photos: 'http://localhost:3003/rooms:id',
-};
